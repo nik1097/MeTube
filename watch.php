333950a551a9447d3fb8f28a9788a7958673b89c
Devinlilaramani <83699188+Devinlilaramani@users.noreply.github.com>
2021-11-16 18:51:49 -0500

    Update watch.php

--- watch.php
+++ watch.php
@@ -63,6 +63,65 @@
         <?php echo round($avg,2) ?>
     </div>
 
+<script src="http://code.jquery.com/jquery-3.4.0.min.js" integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous">
+</script>
+    <script>
+        var ratedIndex = -1, uID = 0;
+
+        $(document).ready(function () {
+            resetStarColors();
+
+            if (localStorage.getItem('ratedIndex') != null) {
+                setStars(parseInt(localStorage.getItem('ratedIndex')));
+                uID = localStorage.getItem('uID');
+            }
+
+            $('.fa-star').on('click', function () {
+               ratedIndex = parseInt($(this).data('index'));
+               localStorage.setItem('ratedIndex', ratedIndex);
+               saveToTheDB();
+            });
+
+            $('.fa-star').mouseover(function () {
+                resetStarColors();
+                var currentIndex = parseInt($(this).data('index'));
+                setStars(currentIndex);
+            });
+
+            $('.fa-star').mouseleave(function () {
+                resetStarColors();
+
+                if (ratedIndex != -1)
+                    setStars(ratedIndex);
+            });
+        });
+
+        function saveToTheDB() {
+            $.ajax({
+               url: "rating.php",
+               method: "POST",
+               dataType: 'json',
+               data: {
+                   save: 1,
+                   uID: $loggedInUserName,
+                   ratedIndex: ratedIndex
+               }, success: function (r) {
+                    uID = r.id;
+                
+               }
+            });
+        }
+
+        function setStars(max) {
+            for (var i=0; i <= max; i++)
+                $('.fa-star:eq('+i+')').css('color', 'green');
+        }
+
+        function resetStarColors() {
+            $('.fa-star').css('color', 'white');
+        }
+    </script>
+
 <?php
     echo "<div style='padding-top:20px;' ><h3>Comment Section</h3></div>";
     if(isset($_GET["Id"])){
